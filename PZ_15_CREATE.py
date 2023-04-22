f27c9e36e4e8f2d6da5b5b0493f308bd0c0f0829
Danil Kostin <114452500+DanilPorkchop15@users.noreply.github.com>
2023-04-22 16:03:02 +0300

    Update PZ_15_CREATE.py

--- PZ_15_CREATE.py
+++ PZ_15_CREATE.py
@@ -75,14 +75,4 @@ with sq.connect('deanary.db') as con:
     )""")
 
 
-# Заполнение таблиц
-    cur.executemany("INSERT INTO faculties VALUES (?, ?)", info_faculties)
-    cur.executemany("INSERT INTO departments VALUES (?, ?, ?)", info_departments)
-    cur.executemany("INSERT INTO specialities VALUES (?, ?, ?)", info_specialities)
-    cur.executemany("INSERT INTO subjects VALUES (?, ?)", info_subjects)
-    cur.executemany("INSERT INTO submission_form VALUES (?, ?)", info_submission_form)
-    cur.executemany("INSERT INTO syllabus VALUES (?, ?, ?, ?, ?, ?, ?, ?)", info_syllabus)
-    cur.executemany("INSERT INTO applicants VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", info_applicants)
-    cur.executemany("INSERT INTO study_card VALUES (?, ?, ?, ?, ?, ?, ?)", info_study_card)
-    
 
